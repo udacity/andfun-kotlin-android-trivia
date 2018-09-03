11de49eb2089bdfc9cc655476b59424bd46b6d3f
Dan Galpin <dgalpin@google.com>
2018-09-03 01:08:02 -0700

    Step.03-Exercise-Conditional-Navigation

--- app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -98,9 +98,15 @@ class GameFragment : Fragment() {
                         binding.invalidateAll()
                     } else {
                         // We've won!  Navigate to the gameWonFragment.
+                        // TODO (05) Find the navController from the view and navigate to the gameWonFragment
+                        // call view.findNavController
+                        // navigate to R.id.action_gameFragment_to_gameWonFragment
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
+                    // TODO (06) Find the navController from the view and navigate to the gameOverFragment
+                    // call view.findNavController
+                    // navigate to R.id.action_gameFragment_to_gameOverFragment
                 }
             }
         }
