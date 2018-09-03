eae07f27cc85098674a2bac6763ce78e8dbeeb83
Dan Galpin <dgalpin@google.com>
2018-09-03 01:21:26 -0700

    Step.05-Exercise-Adding-Support-for-the-Up-Button

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -32,5 +32,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        // TODO (01) Find the navController from myNavHostFragment
+        // Since we're using KTX, you can call this.findNavController
+        // TODO (02) Link the navController to our ActionBar
+        // By calling NavigationUI.setupActionBarWithNavController
     }
+
+    // TODO (03) Override onSupportNavigateUp
+    // Find the navController and then call navController.navigateUp
 }
