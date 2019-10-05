3db085aa071872e3083ee7d562f75a49346bb03d
Matija <vorkapicmatija@gmail.com>
2019-10-05 14:17:47 +0200

    Add title fragment and display it in MainActivity

---
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,26 @@
+package com.example.android.navigation
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+        return binding.root
+    }
+
+
+}
