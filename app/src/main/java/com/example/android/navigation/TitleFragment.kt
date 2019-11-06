8d2d68512a75cd025a9f145801456646d0e4ab37
Marta Pancaldi <marta.panc@gmail.com>
2019-11-06 23:02:27 +0000

    Add navigation

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -20,6 +22,19 @@ class TitleFragment : Fragment() {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_title, container, false
         )
+
+/* Use anonymous function and either the findNavController of Navigation or of the view: */
+
+        binding.playButton.setOnClickListener { view: View ->
+//            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+
+/* ... Or let Navigation create the onClick listener directly: */
+
+//        binding.playButton.setOnClickListener {
+//            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+//        }
         return binding.root
     }
 }
