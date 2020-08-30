62d99563f5fd06b0a171802dd66b9b982efefcd4
SudKul <sudhanshu.kulshrestha@gmail.com>
2020-08-30 19:51:21 +0530

    Update deprecated method onOptionsItemSelected() in TitleFragment.kt

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -48,8 +48,7 @@ class TitleFragment : Fragment() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return NavigationUI.onNavDestinationSelected(item!!,
-                view!!.findNavController())
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
                 || super.onOptionsItemSelected(item)
     }
 }
