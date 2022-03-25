a2594e58850474e4a16b713fc111ca93e0e8d16a
richardmmakuaebo <mrichard@youverify.co>
2022-03-25 15:11:11 +0100

    added implicit intent for sharing success step08

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -46,21 +46,65 @@ class GameWonFragment : Fragment() {
             view.findNavController().navigate(
                     GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
-        var args = GameWonFragmentArgs.fromBundle(requireArguments())
-        Toast.makeText(context,
-                "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
-                Toast.LENGTH_LONG).show()
-        // TODO (01) Add setHasOptionsMenu(true)
+
+        //  (01) Add setHasOptionsMenu(true)
         // This allows onCreateOptionsMenu to be called
+        setHasOptionsMenu(true)
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
 
-    // TODO (02) Create getShareIntent method
-    // TODO (03) Create shareSuccess method
-    // TODO (04) Override and fill out onCreateOptionsMenu
-    // Inflate the winner_menu and set the share menu item to invisible if the activity doesn't
-    // resolve
-    // TODO (05) Override onOptionsItemSelected
+//        check to see if the activity resolves to an activity
+        if(null == getShareIntent().resolveActivity(activity!!.packageManager)){
+            //hide the share menu item
+            menu.findItem(R.id.share)?.isVisible = false
+        }
+    }
+
+    //  (02) Create getShareIntent method
+    private fun getShareIntent(): Intent{
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+
+/*        Old way of implicit intent creation not using method chaining
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT,
+            getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+
+        return shareIntent
+*/
+
+//        Better way of implicit intent creation using method chaining
+        return ShareCompat.IntentBuilder.from(requireActivity())
+            .setText(getString(R.string.share_success_text, args.numQuestions, args.numCorrect))
+            .setType("text/plain")
+            .intent
+    }
+
+    /*
+    * Note: When we do not have an app that could handle our implicit intent,
+    * our app could crash and we can simulate this by removing the setType method
+    * in the method chain and when we run the app, we get an ActivityNotFoundException
+    *
+    * We can handle this in various ways, either we show a toast that says there is no app available.
+    * Or we can do something more elegant by checking in the onCreateOptionsMenu method to see if the
+    * intent wil actually resolve to an activity and hide our send menu item if we cannot share*/
+
+    // (03) Create shareSuccess method
+    private fun shareSuccess(){
+        startActivity(getShareIntent())
+    }
+
+    // (05) Override onOptionsItemSelected
     // Call the shareSuccess method when the item id matches R.id.share
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
